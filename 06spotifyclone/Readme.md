4c89205cda52d34200bce15e02d2b87829e9219c
anish77777 <galacticoanish@gmail.com>
2026-07-27 20:43:39 +0545

    feat: initialize project structure with Express server, database connectivity, and base dependencies

---
+++ 06spotifyclone/Readme.md
@@ -0,0 +1,4 @@
+In this app we will jwt based authentication for user, songs, albums, playlist, and many more
+
+We will implement authentication with role based authorization 
+<!-- "Admin", "User" -->
---
+++ 06spotifyclone/src/app.js
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+dotenv.config();
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+
+
+
+export default app;
